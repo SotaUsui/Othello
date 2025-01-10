7b44c1f8e75d9e59700fc9c868b983f09c2bb570
SotaUsui <sota0829clover@gmail.com>
2025-01-10 09:36:47 -0600

    commit

--- app.py
+++ app.py
@@ -10,4 +10,10 @@ def main():
     if request.method == 'GET':
         return render_template('main.html')
 
-    #if request.method == 'POST':
+    if request.method == 'POST':
+        pressed_button = request.form.get('button')  # Get the value of the pressed button
+
+        if pressed_button == 'single':      # starts single player mode
+            return "single player mode"
+        elif pressed_button == 'pvp':       # starts PvP mode
+            return "pvp mode"
