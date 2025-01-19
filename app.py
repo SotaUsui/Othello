1c6b1b26a46a1f779a25eff2d16f8d8d1c0f9421
SotaUsui <sota0829clover@gmail.com>
2025-01-19 01:03:34 -0600

    commit

--- app.py
+++ app.py
@@ -14,6 +14,34 @@ def main():
         pressed_button = request.form.get('button')  # Get the value of the pressed button
 
         if pressed_button == 'single':      # starts single player mode
-            return "single player mode"
+            return redirect(url_for('single'))  # Redirect to the single mode route
         elif pressed_button == 'pvp':       # starts PvP mode
-            return "pvp mode"
+            return redirect(url_for('pvp'))     # Redirect to the PvP mode route
+
+#############################################################################
+# This is for single player mode
+@app.route("single-mode", methods=['GET', 'POST'])
+def single_mode():
+    # draw the board
+    if request.method == 'GET':
+        return render_template('single.html')
+
+    # Get move from user
+    if request.method == 'POST':
+        # check if the move is valid
+
+        # update the move
+
+        # draw the board
+
+        # check if the game end. If it is, jump to result page.
+
+        # move by AI
+
+        # check if the game end. If it is, jump to result page.
+
+        # draw the board
+
+
+########################################################################
+# This is for PvP mode
---
+++ randomAI.py
@@ -0,0 +1,9 @@
+# AI that respond randomly for single player mode
+import gameLogic
+
+# return row and col.
+def randAI(board):
+
+
+
+    return row, col
